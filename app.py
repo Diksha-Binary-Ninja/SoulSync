9fd341b2d38b4df05431ed442b3d735b148f8c8e
Diksha-Binary-Ninja <139619268+Diksha-Binary-Ninja@users.noreply.github.com>
2025-07-04 21:48:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,5 +68,5 @@ def handle_disconnect():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # ✅ Needed for platforms like Render
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
